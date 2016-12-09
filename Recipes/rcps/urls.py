38a68ddb6caba359149bc2e51d663b2af52dd1a1
gylo <lfyuomr-gylo@yandex.ru>
2016-12-09 17:07:10 +0300

    auth

--- Recipes/rcps/urls.py
+++ Recipes/rcps/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^hello', views.hello, name='hello')
+    url(r'^hello', views.hello, name='hello'),
+    url(r'^new_recipe/$', views.hello),
+    url(r'^login/$', views.login),
+    url(r'^logout/$', views.logout),
+    url(r'^register/$', views.register),
 ]
--- Recipes/rcps/views.py
+++ Recipes/rcps/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render_to_response, redirect
+from django.http.response import HttpResponse, Http404
+from django.template.loader import get_template
+from django.template import Context
+from django.contrib import auth
+from django.contrib.auth.forms import UserCreationForm
+from django.template.context_processors import csrf
+from django.core.exceptions import ObjectDoesNotExist
+from django.forms import Form
+import json
 
 # Create your views here.
 
@@ -8,8 +17,49 @@ def index(request):
     context = {
         'name': "POTOMOK"
     }
+    context['username'] = auth.get_user(request).username
     return render(request, 'index.html', context)
 
 
 def hello(request):
     return HttpResponse("YOOOOOOU")
+
+def login(request):
+    context = {}
+    context.update(csrf(request))
+    if request.POST:
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            context['login_error'] = "This user doesn't exist"
+            return render_to_response('login.html', context)
+
+    else:
+        return render_to_response('login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
+
+
+def register(request):
+    context = {}
+    context.update(csrf(request))
+    context['form'] = UserCreationForm()
+    if request.POST:
+        registration_form = UserCreationForm(request.POST)
+        if registration_form.is_valid():
+            newuser = registration_form.save()
+            registration = auth.authenticate(username=registration_form.cleaned_data['username'],
+                                             password=registration_form.cleaned_data['password2'])
+            auth.login(request, registration)
+
+            return redirect('/')
+        else:
+            context['form'] = registration_form
+    return render_to_response('register.html', context)
